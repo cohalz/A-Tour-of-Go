a7671136ae7a1595e34653628a2132d4c1c1bf53
cohalz <cohalzweb@gmail.com>
2017-11-29 15:31:19 +0900

    インタフェースの部分の理解のためコメントとともに追加

---
+++ Methods-and-interfaces/interfaces-are-satisfied-implicitly.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+/*
+interfaceはabstractのようなもの
+M()メソッドが生えている型に対して
+var hoge I = ???が可能になる
+*/
+
+//共通化したいインタフェースを宣言
+//型Iとして受け取り呼び出すにはその型でMの実装が必要
+type I interface {
+	M()
+}
+
+//Mを実装したい型
+type T struct {
+	S string
+}
+
+//MはメソッドなのでT型のレシーバを受け取り実装
+//この実装がないとcannot use T literal (type T) as type I in assignment:
+//	T does not implement I (missing M method)エラーが発生
+func (t T) M() {
+	fmt.Println(t.S)
+}
+
+//T型の変数を作っているがIのインタフェースを実装しているのでIとして受け取れる
+func main() {
+	var i I = T{"hello"}
+	/*
+		i := T{"hello"}
+			var i T = T{"hello"}
+				ももちろん可能
+	*/
+	i.M()
+}
