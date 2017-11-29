282e461c80f325e81dd53a0d007926ccd71e6ae0
cohalz <cohalzweb@gmail.com>
2017-11-29 15:06:58 +0900

    スライスに関してメモコメントを追加

--- Basics/exercise-slices.go
+++ Basics/exercise-slices.go
@@ -3,6 +3,17 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
+	/*
+	hoge [i]intはあまり使わない?
+	上の形式は数字または定数しかできない
+	i := 3(var i = 3)
+	var hoge [i]int
+	=> 不可
+
+	const j = 3
+	var fuga [j]int
+	=> 可
+	*/
 	pic := make([][]uint8, dy)
 
 	for y := 0; y < dy; y++ {
