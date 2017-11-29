21966aa1b8d3d1135078bb317bbd43f69a8ab2a5
cohalz <cohalzweb@gmail.com>
2017-11-29 15:15:32 +0900

    エラーに関してメモコメントを追加

--- Methods-and-interfaces/exercise-errors.go
+++ Methods-and-interfaces/exercise-errors.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 )
 
+//1. 追加したい型に対してエラー型を作る
 type ErrNegativeSqrt float64
 
+//2. 作ったエラー型をレシーバにしてErrorメソッドを実装する
 func (e ErrNegativeSqrt) Error() string {
 	return fmt.Sprintf("cannot Sqrt negative number: %g", float64(e))
 }
 
 func Sqrt(x float64) (float64, error) {
 	if x < 0 {
+		//3. エラーを返したいタイミングでエラー型に包んで返す
 		return x, ErrNegativeSqrt(x)
 	}
 	z := 1.0
@@ -19,6 +22,8 @@ func Sqrt(x float64) (float64, error) {
 		z = z - (z*z-x)/(2*z)
 		fmt.Println(z)
 	}
+
+	//4. エラーが起きなかった場合はnilを返す
 	return z, nil
 }
 
